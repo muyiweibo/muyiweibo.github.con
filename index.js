3c206056c5bc4571a8dd3840218a20860d719fba
muyiweibo <648431458@qq.com>
2016-08-04 18:39:02 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	var minClient=Math.min($(window).width(),640);
-	$("html").css("fontSize",minClient/7.5);
-	$(window).resize(function(){
-		$("html").css("fontSize",minClient/7.5);
-	})
-})
