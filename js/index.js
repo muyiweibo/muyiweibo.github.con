8201064ca359c5f659163721f51bcea228ffd46b
muyiweibo <648431458@qq.com>
2016-08-04 19:20:55 +0800

    yes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-window.onload=function(){
-  alert("您好！欢迎来到我的网站");
-}
+$(document).ready(function(){
+	var minClient=Math.min($(window).width(),640);
+	$("html").css("fontSize",minClient/7.5);
+	$(window).resize(function(){
+		$("html").css("fontSize",minClient/7.5);
+	})
+})
